6e40739ab4aeae885b86f420015b34716055a904
root @ portal <root@portal.arnoldarts.de>
2016-01-30 16:49:11 +0100

    try a lot of debugging. still it won't run inside systemd

--- portal/duply.nix
+++ portal/duply.nix
@@ -2,9 +2,24 @@
 
 {
   systemd.services.duplyamazon = {
-    path = [ pkgs.duply ];
-    environment = { LANG = "C"; HOME="/root"; TMPDIR="/tmp"; };
-    script = "duply amazon backup";
-    startAt = "Sat 13:27:00";
+    path = [ pkgs.duply pkgs.duplicity ];
+    #environment = { LANG = "C"; HOME="/root"; TMPDIR="/tmp"; USER="root"; };
+    serviceConfig = {
+      PrivateTmp = false;
+      PrivateNetwork = false;
+      User = "root";
+      WorkingDirectory = "/root";
+    };
+    script = "duply amazon status 2>&1 ";
+    #startAt = "Sat 4:10:00";
   };
+  #systemd.services.testduply = {
+  #  path = [ pkgs.duply ];
+  #  environment = { LANG = "C"; HOME="/root"; TMPDIR="/tmp"; };
+  #  serviceConfig = {
+  #    privateTmp = false;
+  #    privateNetwork = false;
+  #  };
+  #  script = "set";
+  #};
 }
