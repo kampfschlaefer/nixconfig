d962802cee04c59078582aef1204aaedc1e1201b
root @ portal <root@portal.arnoldarts.de>
2016-01-30 17:37:03 +0100

    got it "fixed" by running duply in a login shell

--- portal/duply.nix
+++ portal/duply.nix
@@ -2,24 +2,14 @@
 
 {
   systemd.services.duplyamazon = {
-    path = [ pkgs.duply pkgs.duplicity ];
-    #environment = { LANG = "C"; HOME="/root"; TMPDIR="/tmp"; USER="root"; };
-    serviceConfig = {
-      PrivateTmp = false;
-      PrivateNetwork = false;
-      User = "root";
-      WorkingDirectory = "/root";
-    };
-    script = "duply amazon status 2>&1 ";
-    #startAt = "Sat 4:10:00";
+    path = [ pkgs.bash pkgs.duply pkgs.duplicity ];
+    #serviceConfig = {
+      #PrivateTmp = false;
+      #PrivateNetwork = false;
+      #User = "root";
+      #WorkingDirectory = "/root";
+    #};
+    script = "bash -l -c \"duply amazon backup\"";
+    startAt = "*-*-* 2:10:00";
   };
-  #systemd.services.testduply = {
-  #  path = [ pkgs.duply ];
-  #  environment = { LANG = "C"; HOME="/root"; TMPDIR="/tmp"; };
-  #  serviceConfig = {
-  #    privateTmp = false;
-  #    privateNetwork = false;
-  #  };
-  #  script = "set";
-  #};
 }
