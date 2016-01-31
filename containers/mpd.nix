725e57bd7328ec51a01ee65973ee1d71a72ad83e
Arnold Krille <arnold@arnoldarts.de>
2016-01-31 23:20:33 +0100

    Define a container for mpd.

---
+++ containers/mpd.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+{
+  fileSystems = {
+    "/var/lib/containers/mpd/media/music" = {
+      device = "/dev/portalgroup/music";
+    };
+  };
+
+  containers.mpd = {
+    privateNetwork = true;
+    hostBridge = "lan";
+    localAddress = "192.168.1.221";
+    localAddress6 = "2001:470:1f0b:1033::6d:7064";
+    config = { config, pkgs, ... }: {
+      networking.firewall.enable = false;
+      services.openssh.enable = true;
+      services.mpd = {
+        enable = true;
+        musicDirectory = "/media/music";
+      };
+    };
+  };
+}
