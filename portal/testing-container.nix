1c230a378d55a8a4dbe9b2678f4bcc63db1634b1
root @ portal <root@portal.arnoldarts.de>
2016-01-31 15:56:04 +0100

    Set ipv6 address for the container

--- portal/testing-container.nix
+++ portal/testing-container.nix
@@ -3,14 +3,17 @@
 {
   containers.testing = {
     privateNetwork = true;
-    localAddress = "192.168.1.220";
+    hostBridge = "lan";
+    localAddress = "192.168.1.220/24";
+    localAddress6 = "2001:470:1f0b:1033:74:6573:7469:6e67/64";
     config = { config, pkgs, ... }: {
       services.openssh.enable = true;
       networking.firewall.enable = false;
-      networking.interfaces.eth0 = {
-        useDHCP = false;
-        ip6 = [{ address = "2001:470:1f0b:1033:74:6573:7469:6e67"; prefixLength = 64; }];
-      };
+      #networking.interfaces.eth0 = {
+      #  useDHCP = false;
+      #  ip6 = [{ address = "2001:470:1f0b:1033:74:6573:7469:6e67"; prefixLength = 64; }];
+      #  #ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
+      #};
     };
   };
 }
