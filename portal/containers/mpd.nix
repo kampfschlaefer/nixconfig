37a4d56e7f99bbcf17e0c259ca9dfb9414ed31dc
Arnold Krille <arnold@arnoldarts.de>
2016-01-31 23:36:38 +0100

    Install mpd, seems as if the service definition doesn't do that.
    also do not run ssh

--- portal/containers/mpd.nix
+++ portal/containers/mpd.nix
@@ -9,13 +9,19 @@
 
   containers.mpd = {
     autoStart = true;
+
     privateNetwork = true;
     hostBridge = "lan";
     localAddress = "192.168.1.221/24";
     localAddress6 = "2001:470:1f0b:1033::6d:7064/64";
+
     config = { config, pkgs, ... }: {
+      environment.systemPackages = with pkgs; [
+        mpd
+      ];
+
       networking.firewall.enable = false;
-      services.openssh.enable = true;
+
       services.mpd = {
         enable = true;
         musicDirectory = "/media/music";
