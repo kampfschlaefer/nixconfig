bf2f17f985a34ffd59622e1d70b1bb2de31fe585
Arnold Krille <arnold@arnoldarts.de>
2016-02-08 11:44:20 +0100

    try the new container config stuff

--- portal/containers/testing.nix
+++ portal/containers/testing.nix
@@ -4,16 +4,16 @@
   containers.testing = {
     privateNetwork = true;
     hostBridge = "lan";
-    localAddress = "192.168.1.220/24";
-    localAddress6 = "2001:470:1f0b:1033:74:6573:7469:6e67/64";
+    /*localAddress = "192.168.1.220/24";*/
+    /*localAddress6 = "2001:470:1f0b:1033:74:6573:7469:6e67/64";*/
     config = { config, pkgs, ... }: {
       services.openssh.enable = true;
       networking.firewall.enable = false;
-      #networking.interfaces.eth0 = {
-      #  useDHCP = false;
-      #  ip6 = [{ address = "2001:470:1f0b:1033:74:6573:7469:6e67"; prefixLength = 64; }];
-      #  #ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
-      #};
+      networking.interfaces.eth0 = {
+        useDHCP = false;
+        ip6 = [{ address = "2001:470:1f0b:1033:74:6573:7469:6e67"; prefixLength = 64; }];
+        ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
+      };
     };
   };
 }
