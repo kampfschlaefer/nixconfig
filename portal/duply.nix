052daeca2a389b4891e035997052acb9012eabf2
Arnold Krille <arnold@arnoldarts.de>
2016-02-08 13:16:33 +0100

    try automounting the duply disk

--- portal/duply.nix
+++ portal/duply.nix
@@ -1,6 +1,18 @@
 { config, pkgs, ... }:
 
 {
+  systemd.mounts = [
+    {
+      what = "/dev/disk/by-uuid/4bff6ea6-a6f4-4163-9a7e-1703c84cb769";
+      where = "/media/duplydisk";
+    }
+  ];
+  systemd.automounts = [
+    {
+      where = "/media/duplydisk";
+    }
+  ];
+
   systemd.services.duplyamazon = {
     path = [ pkgs.bash pkgs.duply pkgs.duplicity ];
     serviceConfig = {
