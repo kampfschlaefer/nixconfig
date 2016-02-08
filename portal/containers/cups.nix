c161723ec1fa610c784a7a471e23b1297180ac42
Arnold Krille <arnold@arnoldarts.de>
2016-02-08 17:02:54 +0100

    allow accessing cups from the local network

--- portal/containers/cups.nix
+++ portal/containers/cups.nix
@@ -6,11 +6,14 @@
 
     privateNetwork = true;
     hostBridge = "lan";
-    localAddress = "192.168.1.222/24";
-    localAddress6 = "2001:470:1f0b:1033::6375:7073/64";
 
     config = { config, pkgs, ... }: {
       networking.domain = "lan.arnoldarts.de";
+      networking.interfaces.eth0 = {
+        useDHCP = false;
+        ip6 = [{ address = "2001:470:1f0b:1033::6375:7073"; prefixLength = 64; }];
+        ip4 = [{ address = "192.168.1.222"; prefixLength = 24; }];
+      };
       networking.firewall.allowPing = true;
       networking.firewall.allowedTCPPorts = [ 631 ];
 
@@ -21,6 +24,11 @@
         browsing = true;
         extraConf = ''
           ServerAlias *
+          <Location />
+            Order allow,deny
+            Allow from 192.168.1.0/24
+            Allow from 2001:470:1f0b:1033::/64
+          </Location>
         '';
       };
     };
