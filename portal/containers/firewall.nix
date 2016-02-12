3eaa7d15ea3d9e2c0c3b079bb12583a7ddca3c72
Arnold Krille <arnold@arnoldarts.de>
2016-02-12 13:33:00 +0100

    fix interface name inside the firewall container. and activate forwarding on the host, the container isn't allowed to do that.

--- portal/containers/firewall.nix
+++ portal/containers/firewall.nix
@@ -19,7 +19,7 @@
         ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
       };
-      networking.interfaces.eth1 = {
+      networking.interfaces.eno2 = {
         useDHCP = false;
         #ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.2.220"; prefixLength = 24; }];
@@ -28,11 +28,6 @@
         allowPing = true;
         rejectPackets = true;
         logRefusedPackets = true;
-        extraPackages = [ pkgs.procps ];
-        extraCommands = ''
-          sysctl net.ipv4.conf.all.forwarding=1
-          sysctl net.ipv6.conf.all.forwarding=1
-        '';
 
         defaultPolicies = { input = "DROP"; forward = "DROP"; output = "DROP"; };
         rules = [
