89a2cc0bbae8e9725c5472e70ba921ab451cf2d8
Arnold Krille <arnold@arnoldarts.de>
2016-02-12 13:14:42 +0100

    fix interfaces

--- portal/containers/firewall.nix
+++ portal/containers/firewall.nix
@@ -19,7 +19,7 @@
         ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
       };
-      networking.interfaces.eth0 = {
+      networking.interfaces.eth1 = {
         useDHCP = false;
         #ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.2.220"; prefixLength = 24; }];
