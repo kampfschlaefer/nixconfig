bf7d9f5cbc14dfa88078d5390ebd5093a8f58bdc
Arnold Krille <arnold@arnoldarts.de>
2016-02-12 14:49:16 +0100

    yet another interface-name-fix

--- portal/containers/firewall.nix
+++ portal/containers/firewall.nix
@@ -27,12 +27,12 @@ in
       networking.defaultGateway = "192.168.2.10";
 
       networking.interfaces = {
-        lanIf = {
+        eth0 = {
           useDHCP = false;
           ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
           ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
         };
-        dmzIf = {
+        eno2 = {
           useDHCP = false;
           #ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
           ip4 = [{ address = "192.168.2.220"; prefixLength = 24; }];
