17dc323e18f3618df36f82e58063abf836446200
Arnold Krille <arnold@arnoldarts.de>
2016-02-12 14:07:43 +0100

    firewall: move interfaces around, allow http(s) from lan to dmz

--- portal/containers/firewall.nix
+++ portal/containers/firewall.nix
@@ -15,12 +15,17 @@
 
       networking.domain = "arnoldarts.de";
 
-      networking.interfaces.eth0 = {
+      networking.bridges = {
+        dmz = { interfaces = [ "eno2" ]; };
+        lan = { interfaces = [ "eth0" ]; };
+      };
+
+      networking.interfaces.lan = {
         useDHCP = false;
         ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
       };
-      networking.interfaces.eno2 = {
+      networking.interfaces.dmz = {
         useDHCP = false;
         #ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.2.220"; prefixLength = 24; }];
@@ -35,17 +40,31 @@
         rules = [
           { fromInterface = "lo"; target = "ACCEPT"; }
           { toInterface = "lo"; target = "ACCEPT"; }
-          {
+          /*{
             fromInterface = "eth0"; protocol = "tcp"; destinationPort = "22"; target = "ACCEPT";
+          }*/
+          {
+            fromInterface = "lan";
+            toInterface = "dmz";
+            protocol = "tcp";
+            destinationPort = "80";
+            target = "ACCEPT";
+          }
+          {
+            fromInterface = "lan";
+            toInterface = "dmz";
+            protocol = "tcp";
+            destinationPort = "443";
+            target = "ACCEPT";
           }
         ];
       };
 
-      services.openssh = {
+      /*services.openssh = {
         enable = true;
         allowSFTP = true;
         startWhenNeeded = true;
-      };
+      };*/
     };
   };
 }
