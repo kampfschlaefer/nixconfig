f90b8e7c9117653079c84f3896e38ef6ac1b8c07
Arnold Krille <arnold@arnoldarts.de>
2016-02-12 14:26:02 +0100

    sadly the device-dependencies do not allow bridges inside the containers to work…

--- portal/containers/firewall.nix
+++ portal/containers/firewall.nix
@@ -15,17 +15,17 @@
 
       networking.domain = "arnoldarts.de";
 
-      networking.bridges = {
+      /*networking.bridges = {
         dmz = { interfaces = [ "eno2" ]; };
         lan = { interfaces = [ "eth0" ]; };
-      };
+      };*/
 
-      networking.interfaces.lan = {
+      networking.interfaces.eth0 = {
         useDHCP = false;
         ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.1.220"; prefixLength = 24; }];
       };
-      networking.interfaces.dmz = {
+      networking.interfaces.eno2 = {
         useDHCP = false;
         #ip6 = [{ address = "2001:470:1f0b:1033:6669:7265:7761:6c6c"; prefixLength = 64; }];
         ip4 = [{ address = "192.168.2.220"; prefixLength = 24; }];
