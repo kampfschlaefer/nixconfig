4e755382daf7f152b1a9606f7f7e25151f883d2c
Arnold Krille <arnold@arnoldarts.de>
2016-07-26 23:37:46 +0200

    Make the touchpad work.
    and some other small things like allowing unfree drivers.

--- orinoco/default.nix
+++ orinoco/default.nix
@@ -7,6 +7,8 @@
     ../lib/users/arnold.nix
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   networking.hostName = lib.mkOverride 10 "orinoco";
   networking.useDHCP = true;
   networking.enableIPv6 = true;
@@ -14,6 +16,8 @@
   networking.networkmanager.enable = false;
   networking.wicd.enable = true;
   services.hostapd.enable = false;
+
+  users.users.arnold.extraGroups = [ "networkmanager" ];
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -30,6 +34,10 @@
 
   services.xserver.enable = true;
   services.xserver.layout = "de";
+  services.xserver.synaptics = {
+    enable = true;
+    twoFingerScroll = true;
+  };
   services.xserver.desktopManager.kde5.enable = true;
   services.xserver.displayManager.kdm.enable = true;
 }
