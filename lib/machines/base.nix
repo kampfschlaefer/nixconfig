7ad5291d45c25854134cfa4779cf17d6202f6bee
Arnold Krille <arnold@arnoldarts.de>
2016-08-07 13:32:34 +0200

    try to set the login prompt as I want it

--- lib/machines/base.nix
+++ lib/machines/base.nix
@@ -32,7 +32,24 @@
     vi = "vim";
   };
 
-  programs.bash.enableCompletion = true;
+  programs.bash = {
+    enableCompletion = true;
+    shellInit = ''
+      export GIT_PS1_SHOWDIRTYSTATE=true
+      export GIT_PS1_SHOWUPSTREAM=true
+    '';
+    promptInit = ''
+      # Provide a nice prompt if the terminal supports it.
+        if [ "$TERM" != "dumb" -o -n "$INSIDE_EMACS" ]; then
+          PROMPT_COLOR="1;31m"
+          let $UID && PROMPT_COLOR="1;32m"
+          PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\[\033[0m\]$(__git_ps1 )\\$ "
+          if test "$TERM" = "xterm"; then
+            PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+          fi
+        fi
+    '';
+  };
   environment.sessionVariables = {
     EDITOR = "vim";
   };
