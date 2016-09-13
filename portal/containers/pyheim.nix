0542f093b0c75cfcfb6d20552b21bc8446001c1c
Arnold Krille <arnold@arnoldarts.de>
2016-09-13 21:49:25 +0200

    run the pyheim timers more often

--- portal/containers/pyheim.nix
+++ portal/containers/pyheim.nix
@@ -34,10 +34,10 @@ in
       environment.systemPackages = [ pyheimpkg ];
 
       systemd.services.pyheim_colortemp_daytime = pyheim_timer // {
-        startAt = "*-*-* 17,18,19,20,21,22,23:00:00";
+        startAt = "*-*-* 17,18,19,20,21,22,23:*:00";
       };
       systemd.services.pyheim_colortemp_night = pyheim_timer // {
-        startAt = "*-*-* 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17:01/5:00";
+        startAt = "*-*-* 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17:00/5:00";
       };
     };
   };
