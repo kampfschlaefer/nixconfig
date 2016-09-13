57697dc8ce9011348dcaa99de35d8cdcc556ad37
Arnold Krille <arnold@arnoldarts.de>
2016-09-13 21:38:58 +0200

    Add services and timers for colortemp

--- portal/containers/pyheim.nix
+++ portal/containers/pyheim.nix
@@ -2,6 +2,14 @@
 
 let
   pyheimpkg = pkgs.callPackage ../../lib/software/pyheim {};
+
+  pyheim_timer = {
+    serviceConfig = {
+      User = "root";
+      WorkingDirectory = "/root";
+    };
+    script = "${pyheimpkg}/bin/pyheim_colortemp --transitiontime 20 0";
+  };
 in
 {
   containers.pyheim = {
@@ -25,6 +33,12 @@ in
 
       environment.systemPackages = [ pyheimpkg ];
 
+      systemd.services.pyheim_colortemp_daytime = pyheim_timer // {
+        startAt = "*-*-* 17,18,19,20,21,22,23:00:00";
+      };
+      systemd.services.pyheim_colortemp_night = pyheim_timer // {
+        startAt = "*-*-* 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17:01/5:00";
+      };
     };
   };
 }
