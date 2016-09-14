1d64a40519e690ccb6244b1eaef5482f585abf4a
Arnold Krille <arnold@arnoldarts.de>
2016-09-14 22:02:14 +0200

    only one of the timers needs to run at 17:*

--- portal/containers/pyheim.nix
+++ portal/containers/pyheim.nix
@@ -38,7 +38,7 @@ in
         startAt = "*-*-* 17,18,19,20,21,22,23:*:00";
       };
       systemd.services.pyheim_colortemp_night = pyheim_timer // {
-        startAt = "*-*-* 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17:00/5:00";
+        startAt = "*-*-* 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16:00/5:00";
       };
       systemd.services.pyheim_spots_off = pyheim_timer // {
         startAt = "*-*-* 1:00,05:00";
