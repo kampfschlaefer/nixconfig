6e3882d86d1bc60c56581ec2cff3770b7c6cea7b
Arnold Krille <arnold@arnoldarts.de>
2016-12-04 11:58:19 +0100

    make pyheim build work with sandbox

--- lib/software/pyheim/default.nix
+++ lib/software/pyheim/default.nix
@@ -11,10 +11,23 @@ in python.mkDerivation {
     rev = "7e4c5bbd39402fae8faf9de2373c1d1a367a4e7e";
     sha256 = "1pxdcyhcm9hm46374vk5wy0wnm6pflydpr71a6kx1rqbz1fv64jh";
   };
-  buildInputs = [ pkgs.openssh ];
+  doCheck = false;
+  checkPhase = ''
+    export NO_TESTS_OVER_WIRE=1
+    export PYTHONDONTWRITEBYTECODE=1
+
+    #flake8 pyheim
+    #py.test --cov=pyheim -cov-report term-missing
+    #coverage html
+  '';
+  buildInputs = [
+    pkgs.openssh
+    python.packages."pytest-runner"
+  ];
   propagatedBuildInputs = [
     python.packages."configparser"
     python.packages."future"
     python.packages."phue"
   ];
+  /*propagatedBuildInputs = builtins.attrValues python.packages;*/
 }
--- lib/software/pyheim/requirements_override.nix
+++ lib/software/pyheim/requirements_override.nix
@@ -1,5 +1,7 @@
 { pkgs, python }:
 
 self: super: {
-
+  "pytest-runner" = python.overrideDerivation super."pytest-runner" (old: {
+    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
+  });
 }
