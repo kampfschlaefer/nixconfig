a74bc64744e97a74271e1718888711815773bede
Arnold Krille <arnold@arnoldarts.de>
2016-12-09 21:44:47 +0100

    adopt orinoco config

--- orinoco/hardware-configuration.nix
+++ orinoco/hardware-configuration.nix
@@ -4,9 +4,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    ];
+  imports = [
+    ../nixpkgs/nixos/modules/installer/scan/not-detected.nix
+  ];
 
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.kernelModules = [ "wl" ];
