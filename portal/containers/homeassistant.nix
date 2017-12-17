ad09a1d8f1eec9e2fba14eb337aaeeaa6db40edc
Arnold Krille <arnold@arnoldarts.de>
2017-12-17 15:21:42 +0100

    add overrides for acme-client

--- portal/containers/homeassistant.nix
+++ portal/containers/homeassistant.nix
@@ -20,6 +20,25 @@ in
       imports = [
         ../../lib/software/homeassistant/service.nix
       ];
+      nixpkgs.config.packageOverrides = pkgs: rec {
+        simp_le = pkgs.simp_le.overrideDerivation (oldAttrs: {
+          version = "0.6.1";
+          src = pkgs.pythonPackages.fetchPypi {
+            pname = "simp_le-client";
+            version = "0.6.1";
+            sha256 = "0x4fky9jizs3xi55cdy217cvm3ikpghiabysan71b07ackkdfj6k";
+          };
+        });
+        certbot = pkgs.certbot.overrideDerivation (oldAttrs: {
+          version = "0.19.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "certbot";
+            repo = "certbot";
+            rev = "v0.19.0";
+            sha256 = "14i3q59v7j0q2pa1dri420fhil4h0vgl4vb471hp81f4y14gq6h7";
+          };
+        });
+      };
 
       time.timeZone = "Europe/Berlin";
 
