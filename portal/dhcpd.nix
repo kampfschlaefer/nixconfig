a06c75d1e6832d5fb0723c4f76bf4140b21abc19
Arnold Krille <arnold@arnoldarts.de>
2018-08-06 21:44:16 +0200

    add one more dash button

--- portal/dhcpd.nix
+++ portal/dhcpd.nix
@@ -53,7 +53,7 @@ let
     { hostName = "dash_button2";  ethernetAddress = "18:74:2e:de:83:cd"; noroute=true; }
     { hostName = "dash_button3";  ethernetAddress = "6c:56:97:d3:ef:f4"; noroute=true; }
     #{ hostName = "dash_button4";  ethernetAddress = "84:cf:bf:8a:6b:fd"; }
-    { hostName = "dash_button5"; ethernetAddress = "fc:65:de:fa:0c:10"; }
+    { hostName = "dash_button5";  ethernetAddress = "fc:65:de:fa:0c:10"; }
 
     { hostName = "firestick";     ethernetAddress = "34:d2:70:04:0b:4d"; ipAddress = "192.168.1.80"; }
     { hostName = "denon";         ethernetAddress = "00:05:cd:90:09:4d"; ipAddress = "192.168.1.81"; }
