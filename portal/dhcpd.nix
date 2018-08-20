b9c1671c673971402a566f2613bdf9ee0db952c3
Arnold Krille <arnold@arnoldarts.de>
2018-08-20 23:20:25 +0200

    go back to giving the dash buttons a route, makes them fail faster

--- portal/dhcpd.nix
+++ portal/dhcpd.nix
@@ -49,11 +49,11 @@ let
     { hostName = "steuer";        ethernetAddress = "08:00:27:1f:06:82"; }
     { hostName = "arduino";       ethernetAddress = "18:fe:34:cf:a7:26"; }
     { hostName = "luftdaten";     ethernetAddress = "5c:cf:7f:76:fb:38"; }
-    { hostName = "dash-button1";  ethernetAddress = "ac:63:be:be:01:93"; noroute=true; nodns=true; }
-    { hostName = "dash_button2";  ethernetAddress = "18:74:2e:de:83:cd"; noroute=true; nodns=true; }
-    { hostName = "dash_button3";  ethernetAddress = "6c:56:97:d3:ef:f4"; noroute=true; nodns=true; }
-    { hostName = "dash_button4";  ethernetAddress = "78:e1:03:74:76:4f"; noroute=true; nodns=true; }
-    { hostName = "dash_button5";  ethernetAddress = "fc:65:de:fa:0c:10"; noroute=true; nodns=true; }
+    { hostName = "dash-button1";  ethernetAddress = "ac:63:be:be:01:93"; /*noroute=true; nodns=true;*/ }
+    { hostName = "dash_button2";  ethernetAddress = "18:74:2e:de:83:cd"; /*noroute=true; nodns=true;*/ }
+    { hostName = "dash_button3";  ethernetAddress = "6c:56:97:d3:ef:f4"; /*noroute=true; nodns=true;*/ }
+    { hostName = "dash_button4";  ethernetAddress = "78:e1:03:74:76:4f"; /*noroute=true; nodns=true;*/ }
+    { hostName = "dash_button5";  ethernetAddress = "fc:65:de:fa:0c:10"; /*noroute=true; nodns=true;*/ }
 
     { hostName = "firestick";     ethernetAddress = "34:d2:70:04:0b:4d"; ipAddress = "192.168.1.80"; }
     { hostName = "denon";         ethernetAddress = "00:05:cd:90:09:4d"; ipAddress = "192.168.1.81"; }
@@ -144,8 +144,6 @@ in {
             }
           }
           group { # known clients without route
-            option routers 192.168.1.21;
-
             ${lib.concatMapStrings
               (machine:
                 ''
